533dd100a13d113079237845ab7376c627c01ce2
Arthur Monteiro Jurotschko <arthur.jurotschko@hotmail.com>
2025-05-10 21:28:14 -0300

    Configuração

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { BoasVindasComponent } from './pages/boas-vindas/boas-vindas.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { CadastroEmpresaComponent } from './pages/cadastro-empresa/cadastro-empresa.component';
 
 export const routes: Routes = [
+  { path: 'boas-vindas', component: BoasVindasComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: '', redirectTo: '/cadastro', pathMatch: 'full' }
+  { path: 'cadastro-empresa', component: CadastroEmpresaComponent },
+  { path: '', redirectTo: '/boas-vindas', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/boas-vindas' } // Redireciona para Boas-Vindas se a rota não existir
 ];
